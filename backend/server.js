5125698ab0ef5471e22a64d1097e698b3f77e552
Potatomochi <noahrsam@gmail.com>
2021-07-02 20:42:48 +0800

    server stuff

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import path from 'path';
 import noteRoute from "./routes/NoteRoute.js";
 import userRoute from "./routes/UserRoute.js";
 
 // setup
+
+const __dirname = path.resolve();
 dotenv.config();
 const app = express();
 const port = process.env.PORT ;
@@ -35,9 +38,10 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/' , {
 app.use("/api/notes", noteRoute)
 app.use("/api/users", userRoute)
 
-app.get('/',(req,res) => {
-  res.send('Server is ready');
-});
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.use((err,req,res,next) =>{
   res.status(500).send({ message: err.message });
